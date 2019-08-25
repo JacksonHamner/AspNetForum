978dba43262ff6b6299cea8549f92383c254479a
jackson hamner <jacksonhamner@gmail.com>
2019-08-25 15:28:23 -0700

    added forum service

---
+++ AspNetForum/AspNetForum.Data/Interfaces/IForum.cs
@@ -0,0 +1,18 @@
+﻿using AspNetForum.Data.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AspNetForum.Data.Interfaces
+{
+    public interface IForum
+    {
+        Forum GetById(int id);
+        IEnumerable<Forum> GetAll();
+        IEnumerable<ApplicationUser> GetAllActiveUsers();
+
+        Task Create(Forum forum);
+        Task Delete(int id);
+        Task UpdateForumTitle(int id, string title);
+        Task UpdateForumDescription(int id, string description);
+    }
+}
---
+++ AspNetForum/AspNetForum.Service/ForumService.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using AspNetForum.Data;
+using AspNetForum.Data.Interfaces;
+using AspNetForum.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetForum.Service
+{
+    public class ForumService : IForum
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ForumService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task Create(Forum forum)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Delete(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Forum> GetAll()
+        {
+            return _context.Forums
+                .Include(forum => forum.Posts);
+        }
+
+        public IEnumerable<ApplicationUser> GetAllActiveUsers()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Forum GetById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task UpdateForumDescription(int id, string description)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task UpdateForumTitle(int id, string title)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
