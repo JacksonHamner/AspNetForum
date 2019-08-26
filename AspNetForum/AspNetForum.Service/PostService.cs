2b6e19d088cf3dc291f6088e8516f97a6e7d2ff8
jackson hamner <jacksonhamner@gmail.com>
2019-08-25 21:06:56 -0700

    add post controller, service method

--- AspNetForum/AspNetForum.Service/PostService.cs
+++ AspNetForum/AspNetForum.Service/PostService.cs
@@ -44,7 +44,13 @@ namespace AspNetForum.Service
 
         public Post GetById(int id)
         {
-            throw new NotImplementedException();
+            return _context.Posts
+                .Where(post => post.Id == id)
+                .Include(post => post.User)
+                .Include(post => post.Replies)
+                    .ThenInclude(reply => reply.User)
+                .Include(post => post.Forum)
+                .First();
         }
 
         public IEnumerable<Post> GetFilteredPosts(string searchQuery)
---
+++ AspNetForum/AspNetForum/ViewModels/Post/PostIndexModel.cs
@@ -0,0 +1,23 @@
+﻿using AspNetForum.ViewModels.Reply;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetForum.ViewModels.Post
+{
+    public class PostIndexModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string AuthorName { get; set; }
+        public string AuthorId { get; set; }
+        public string AuthorImageUrl { get; set; }
+        public int AuthorRating { get; set; }
+        public DateTime Created { get; set; }
+        public string PostContent { get; set; }
+
+        public IEnumerable<PostReplyModel> Replies { get; set; }
+
+    }
+}
