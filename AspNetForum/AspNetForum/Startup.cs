3d7efe87b93020f9fffde64d5ac1fc3b49aa472d
jackson hamner <jacksonhamner@gmail.com>
2019-08-28 16:08:24 -0700

    Adding Identity Views

--- AspNetForum/AspNetForum/Startup.cs
+++ AspNetForum/AspNetForum/Startup.cs
@@ -71,6 +71,10 @@ namespace AspNetForum
 
             app.UseMvc(routes =>
             {
+                //routes.MapRoute(
+                //    name: "MyArea",
+                //    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
